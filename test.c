eb9bca2e6a3d02e76e29dda38df67d91ba87a826
Acacumly <985514515@qq.com>
2019-04-01 23:45:47 +0800

    Update test.c

--- test.c
+++ test.c
@@ -1,14 +1,11 @@
-//²ÂÊý×Ö
+//çŒœæ•°å­—
+#define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 #include<stdlib.h>
 int main() {
 	int a;
 	scanf("%d", &a);
-
-	
-	
-	printf("%d", a);
-	
+	printf("%d\n", a);
 	system("pause");
 	return 0;
 }
