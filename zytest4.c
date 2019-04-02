5423c2bcba53014efe48969d0a3b7f579acf75f1
Acacumly <985514515@qq.com>
2019-04-03 00:14:39 +0800

    转换大小写,不输出数字

---
+++ zytest4.c
@@ -0,0 +1,32 @@
+//×ª»»´óÐ¡Ð´
+#include <stdio.h>
+#include <ctype.h>
+#define isBigLetter(ch) (ch <= 'Z' && ch >= 'A')
+int isSmallLetter(char ch) {
+	return ch <= 'z' && ch >= 'a';
+}
+
+int main() {
+	char ch;
+	while (1) {
+		ch = getchar();
+		if (ch == '/') {
+			break;
+		}
+		if (isSmallLetter(ch)) {
+			putchar(ch - 32);
+		}
+		else if (isBigLetter(ch)) {
+			putchar(ch + 32);
+		}
+		else if (isalnum(ch)) {
+			//do nothing
+		}
+		else {
+			putchar(ch);
+		}
+	}
+	system("pause");
+	return 0;
+
+}
