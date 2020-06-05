7f9b740737f638efa5d21ac5fc6fc838aebaa317
Acacumly <985514515@qq.com>
2020-06-05 23:26:20 +0800

    Student

---
+++ Student.java
@@ -0,0 +1,67 @@
+/*
+package Test;
+
+public class Student {
+    //generate
+    private Integer id;
+
+    private Integer sn;
+
+    private String name;
+
+    private String qqMail;
+
+    private Integer  classesId;
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", sn=" + sn +
+                ", name='" + name + '\'' +
+                ", qqMail='" + qqMail + '\'' +
+             9     ", classesId=" + classesId +
+                '}';
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getSn() {
+        return sn;
+    }
+
+    public void setSn(Integer sn) {
+        this.sn = sn;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getQqMail() {
+        return qqMail;
+    }
+
+    public void setQqMail(String qqMail) {
+        this.qqMail = qqMail;
+    }
+
+    public Integer getClassesId() {
+        return classesId;
+    }
+
+    public void setClassesId(Integer classesId) {
+        this.classesId = classesId;
+    }
+}
+*/
