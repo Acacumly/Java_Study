20775ce9fae42acb2c4d8627fa5afd99eda37306
Acacumly <985514515@qq.com>
2020-06-15 22:55:37 +0800

    1

---
+++ Demo_Random.java
@@ -0,0 +1,10 @@
+import java.util.Random;
+
+class Demo_Random {
+    public static void main(String[] args) {
+        Random random = new Random();
+        //产生int类型的随机数, 范围: 1-6
+        int value = random.nextInt(6)+1;
+        System.out.println("掷了一个" + value + "点");
+    }
+}
---
+++ Game.java
@@ -0,0 +1,61 @@
+/*
+猜拳游戏
+参与A: 用户
+参与B: 电脑
+
+剪刀(0) 石头(1) 布(2)
+
+石头->剪刀->布
+
+实现思路:
+1.用户出拳
+2.机器出拳
+3.裁判输赢
+ */
+import java.util.Scanner;
+import java.util.Random;
+class Game {
+    public static void main(String[] args) {
+        System.out.println("请出拳: 剪刀(0)/石头(1)/步(2)");
+        Scanner in = new Scanner(System.in);
+        //接收用户出拳
+        int user = in.nextInt();
+
+        /**机器出拳*/
+        Random random = new Random();
+        //随机产生0,1,2数值
+        int computer = random.nextInt(3);
+        if(user == 0 && computer == 2 || user == 1 && computer == 0
+        || user == 2 && computer == 1) {
+            System.out.println("你赢了");
+        } else  if(user == computer) {
+            System.out.println("平局");
+        } else {
+            System.out.println("你输了!!!");
+        }
+        //存储机器与用户的出拳结果
+        String computerText = "";
+        String userText = "";
+        switch (user) {
+            case 0:
+                userText = "剪刀";
+                break;
+            case 1:
+                userText = "石头";
+                break;
+            case 2:
+                userText = "布";
+                break;
+        }
+        switch(computer) {
+            case 0:
+                computerText = "剪刀";
+            case 1:
+                computerText = "石头";
+            case 2:
+                computerText  = "布";
+                break;
+        }
+        System.out.println("用户: "+ userText + "  机器:" + computerText);
+    }
+}
