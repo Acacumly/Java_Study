518cfd78c10e8bb2871b7b5af2f01038840f528f
Acacumly <985514515@qq.com>
2020-06-28 22:57:50 +0800

    1

---
+++ FileInputStreamTest.java
@@ -0,0 +1,89 @@
+//import org.junit.Test;
+//import java.io.File;
+//import java.io.FileInputStream;
+//
+//public class FileInputStreamTest {
+//
+//    @Test
+//    public void test1(){
+//        try {
+//            System.out.println(System.getProperty("use.dir"));
+//            File f = new File("src/text1.txt");
+//            System.out.println(f.exists());
+//            FileInputStream fis = new FileInputStream(f);
+//            byte[] bytes = new byte[1024];
+//            int len = 0;
+//            while((len = fis.read(bytes)) != -1){
+//                String s = new String(bytes, 0,len);
+//                System.out.println(s);
+//            }
+//            fis.close();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
+//
+//
+//    @Test
+//    public void test2(){
+//        try {
+//            System.out.println(System.getProperty("use.dir"));
+//            File f = new File("src/text1.txt");
+//            System.out.println(f.exists());
+////            FileInputStream fis = new FileInputStream(f);
+////            byte[] bytes = new byte[1024];
+//            int len = 0;
+//            while((len = fis.read(bytes)) != -1){
+//                String s = new String(bytes, 0,len);
+//                System.out.println(s);
+//            }
+//            fis.close();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
+//
+//
+//    @Test
+//    public void test3(){
+//        try {
+//            System.out.println(System.getProperty("use.dir"));
+//            File f = new File("src/text1.txt");
+//            System.out.println(f.exists());
+//            FileInputStream fis = new FileInputStream(f);
+//            byte[] bytes = new byte[1024];
+//            int len = 0;
+//            while((len = fis.read(bytes)) != -1){
+//                String s = new String(bytes, 0,len);
+//                System.out.println(s);
+//            }
+//            fis.close();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
+//
+//
+//    @Test
+//    public void test4(){
+//        try {
+//            System.out.println(System.getProperty("use.dir"));
+//            File f = new File("src/text1.txt");
+//            System.out.println(f.exists());
+//            FileInputStream fis = new FileInputStream(f);
+//            byte[] bytes = new byte[1024];
+//            int len = 0;
+//            while((len = fis.read(bytes)) != -1){
+//                String s = new String(bytes, 0,len);
+//                System.out.println(s);
+//            }
+//            fis.close();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
+//}
