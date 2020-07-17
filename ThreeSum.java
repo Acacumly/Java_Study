d5e8beecce692e9345d1d54d9d3c8285ae504788
Acacumly <985514515@qq.com>
2020-07-17 22:21:15 +0800

    1

---
+++ ThreeSum.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+/*
+ *给数组排序,然后从头开始固定一个数字,剩余两个数字使用双指针求出,一个指针指示剩余数据的最小值,一个指示最大值
+ * 根据当前三个数的总和判定连个指针的移动方向
+ *
+ */
+public class ThreeSum {
+    public static List<List<Integer>> threeSum(int[] nums) {
+
+    }
+    public static void main(String[] args) {
+
+    }
+
+}
