568df004b202dc92896802d86b879326a37f6995
Acacumly <985514515@qq.com>
2020-07-20 21:29:44 +0800

    test

---
+++ FileReader.java
@@ -0,0 +1,9 @@
+package test33;
+
+public class FileReader
+{
+    public String read(String fileNameSrc)
+    {
+        return fileNameSrc;
+    }
+}
---
+++ FileWriter.java
@@ -0,0 +1,10 @@
+package test33;
+
+public class FileWriter
+{
+    public void write(String encryptText,String fileNameDes)
+    {
+        System.out.println(encryptText);
+        System.out.println(fileNameDes);
+    }
+}
