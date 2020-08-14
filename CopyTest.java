65ed249fbc2e85b0446efc8ee371f4f4e9fd047b
Acacumly <985514515@qq.com>
2020-08-14 21:35:15 +0800

    test

---
+++ CopyTest.java
@@ -0,0 +1,16 @@
+package Practice;
+
+import java.io.File;
+import java.io.FileInputStream;
+
+public class CopyTest {
+    public void test1(){
+        try {
+            FileInputStream fis = null;
+            FileInputStream fos = null;
+            File f = new File("D:\\比特科技");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
